d3e04d290f6f606562e4e165fbde75e2dc0134de
TakeruShigeki <fruit_ore@icloud.com>
2024-10-11 03:26:43 +0900

    2024/10/10

--- car_license/resources/views/layouts/guest.blade.php
+++ car_license/resources/views/layouts/guest.blade.php
@@ -13,7 +13,7 @@
 
         <!-- Scripts -->
         {{-- @vite(['resources/js/app.js', 'resources/css/app.css']) --}}
-        <link rel="stylesheet" href="/build/assets/app-C8TH9cMt.css">
+        <link rel="stylesheet" href="/build/assets/app-Bme-jh6N.css">
         <script src="/build/assets/app-CSsv4iKa.js" type="module"></script>
     </head>
     <body class="font-sans text-gray-900 antialiased">
