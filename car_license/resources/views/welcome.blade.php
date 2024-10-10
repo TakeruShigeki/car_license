6af49d1d0b20ab860b51c542e65f4806b5a1589e
TakeruShigeki <fruit_ore@icloud.com>
2024-10-11 02:56:31 +0900

    2024/10/10

--- car_license/resources/views/welcome.blade.php
+++ car_license/resources/views/welcome.blade.php
@@ -14,8 +14,8 @@
         <!-- Scripts -->
         {{-- @vite(['resources/js/app.js', 'resources/css/app.css']) --}}
         <link rel="stylesheet" href="/build/assets/app-C8TH9cMt.css">
-        <script src="/build/assets/app-CSsv4iKa.js" type="module"></script>
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        {{-- <script src="/build/assets/app-CSsv4iKa.js" type="module"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
     </head>
     <body class="font-sans antialiased bg-gradient-to-r from-slate-200 via-slate-300 to-slate-400 min-h-screen  items-center justify-center">
         <div class="text-center mb-10 shadow-md">
