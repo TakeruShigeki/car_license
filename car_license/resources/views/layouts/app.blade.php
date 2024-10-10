7fddfdd50d67e875adac2a1001e86a542230c26e
TakeruShigeki <fruit_ore@icloud.com>
2024-10-10 21:42:52 +0900

    2024/10/10 app.balde.phpの内容変更

--- car_license/resources/views/layouts/app.blade.php
+++ car_license/resources/views/layouts/app.blade.php
@@ -12,7 +12,8 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     </head>
     <body class="font-sans antialiased bg-gradient-to-r from-blue-100 via-orange-100 to-yellow-100">
