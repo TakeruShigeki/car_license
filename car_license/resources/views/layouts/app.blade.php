8b07d6e38c34b4558f57615097a344cc88d718e2
TakeruShigeki <fruit_ore@icloud.com>
2024-10-10 22:06:27 +0900

    2024/10/10 appとguest.blade.phpの内容変更

--- car_license/resources/views/layouts/app.blade.php
+++ car_license/resources/views/layouts/app.blade.php
@@ -10,10 +10,10 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
-        <!-- Scripts -->
-        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
         
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     </head>
     <body class="font-sans antialiased bg-gradient-to-r from-blue-100 via-orange-100 to-yellow-100">
--- car_license/resources/views/layouts/guest.blade.php
+++ car_license/resources/views/layouts/guest.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        {{-- @vite(['resources/css/app.js', 'resources/js/app.css']) --}}
+        @vite(['resources/css/app.js', 'resources/js/app.css'])
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
     </head>
     <body class="font-sans text-gray-900 antialiased">
