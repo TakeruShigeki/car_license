927c13874e3630e53625f11a1b63b6b8c31bf084
TakeruShigeki <fruit_ore@icloud.com>
2024-10-11 03:23:13 +0900

    2024/10/10

--- car_license/resources/views/welcome.blade.php
+++ car_license/resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
         
         <!-- Scripts -->
         {{-- @vite(['resources/js/app.js', 'resources/css/app.css']) --}}
-        <link rel="stylesheet" href="/build/assets/app-C8TH9cMt.css">
+        <link rel="stylesheet" href="/build/assets/app-Bme-jh6N.css">
         <script src="/build/assets/app-CSsv4iKa.js" type="module"></script>
         {{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
     </head>
