ebfaf7baf3ff0b1002a5845892916dceb4d5794e
TakeruShigeki <fruit_ore@icloud.com>
2024-10-11 16:02:45 +0900

    2024/10/11 文字が表示できるようになった 原因は余分なdivがあり、そのdivでbackgroundを設定していたため

--- car_license/resources/views/layouts/navigation.blade.php
+++ car_license/resources/views/layouts/navigation.blade.php
@@ -1,6 +1,4 @@
 <nav x-data="{ open: false }">
-    
-<div class="bg-gradient-to-r from-slate-200 via-slate-300 to-slate-400 min-h-screen">
 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="flex justify-between h-16">
         <div class="flex">
